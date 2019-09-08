8db4e618dc36cb177c3e598db2c9003b7295f9d8
Base-PC\RubberFun <jy0001087@gmail.com>
2019-09-08 21:58:18 +0800

    加入jsoup和httpclient，能爬到页面内容，主要是要分析什么.. 要怎么分析..

--- src/main/java/com/leathersheer/tools/Spider/Spider.java
+++ src/main/java/com/leathersheer/tools/Spider/Spider.java
@@ -1,12 +1,7 @@
 package com.leathersheer.tools.Spider;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.jsoup.nodes.*;
 
-public class Spider {
-    public static final Logger workerLogger = LogManager.getLogger();
-    public static void main(String[] args){
-        System.out.println("test  ok");
-        workerLogger.trace("logger start");
-    }
+public abstract class Spider {
+    public abstract  Document getContent();
 }
---
+++ src/main/java/com/leathersheer/tools/Spider/WeiboSpider.java
@@ -0,0 +1,48 @@
+package com.leathersheer.tools.Spider;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class WeiboSpider extends Spider {
+    public static final Logger workerLogger = LogManager.getLogger();
+    public static void main(String[] args){
+        System.out.println("WeiboSpider test  ok!");
+        workerLogger.trace("WeiboSpider logger start!");
+        Spider spider = new WeiboSpider();
+        Document doc = spider.getContent();
+        workerLogger.info(doc.getElementsByClass("movie-list list").toString());
+    }
+
+    @Override
+    public Document getContent() {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        Document doc = new Document("empty element");
+
+        HttpGet httpGet=new HttpGet("https://douban.com/");
+        try {
+            response = httpclient.execute(httpGet);
+            String html=EntityUtils.toString(response.getEntity());
+            doc = Jsoup.parse(html);
+            workerLogger.trace("----------原始页面信息如下----------");
+            workerLogger.trace(html);
+            workerLogger.trace("----------原始页面信息结束----------");
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                response.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        return doc;
+    }
+}
