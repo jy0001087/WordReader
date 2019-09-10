e130da98469f9fa8bcb5f2c29a208e1999af6a94
Base-PC\RubberFun <jy0001087@gmail.com>
2019-09-10 21:30:11 +0800

    可以爬了，下面加个引擎...

--- src/main/java/com/leathersheer/tools/Spider/WeiboSpider.java
+++ src/main/java/com/leathersheer/tools/Spider/WeiboSpider.java
@@ -1,5 +1,6 @@
 package com.leathersheer.tools.Spider;
 
+import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -26,13 +27,21 @@ public class WeiboSpider extends Spider {
         CloseableHttpResponse response = null;
         Document doc = new Document("empty element");
 
-        HttpGet httpGet=new HttpGet("https://douban.com/");
+        HttpGet httpGet=new HttpGet("https://weibo.com");
         try {
+            httpGet.setHeader("User-Agent","spider");
             response = httpclient.execute(httpGet);
+            workerLogger.trace("----------head 信息如下----------");
+            Header[] heads = response.getAllHeaders();
+            for(int i=0;i<heads.length;i++) {
+                workerLogger.trace(heads[i]);
+            }
+            workerLogger.trace("----------head 信息结束----------");
             String html=EntityUtils.toString(response.getEntity());
+            workerLogger.info("页面编码为 ： "+ super.getEncoding(html)+"  "+response.getStatusLine());
             doc = Jsoup.parse(html);
             workerLogger.trace("----------原始页面信息如下----------");
-            workerLogger.trace(html);
+            workerLogger.trace(new String(html.getBytes("UTF-8"),"UTF-8"));
             workerLogger.trace("----------原始页面信息结束----------");
         }catch(Exception e){
             e.printStackTrace();
