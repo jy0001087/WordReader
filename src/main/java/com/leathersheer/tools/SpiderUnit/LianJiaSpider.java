a49b256a96f9f766b5b98f163b8f9f1e152fa935
Surface-RsFun <18697265816@163.com>
2019-10-15 18:57:59 +0800

    获取房屋地址信息从高德获取地址信息加入linkedlist

--- src/main/java/com/leathersheer/tools/SpiderUnit/LianJiaSpider.java
+++ src/main/java/com/leathersheer/tools/SpiderUnit/LianJiaSpider.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+import com.leathersheer.tools.SpiderUnit.PubToolUnit.GaoDeUnit;
 import com.leathersheer.tools.SpiderUnit.SpiderBeans.HouseBean;
 
 import org.apache.logging.log4j.LogManager;
@@ -28,6 +29,7 @@ public class LianJiaSpider extends Spider {
         Elements elements = doc.getElementsByClass("content__list--item");// 筛选页面中包含的房源信息块
         Elements addresselements = new Elements();
         HouseBean house = new HouseBean();
+        GaoDeUnit gaode = new GaoDeUnit();
         LinkedList<HouseBean> houselist = new LinkedList<>();
         if (elements.isEmpty()) {
             LianjiaLogger.error("未找到目标class内容:content__list--item");
@@ -37,36 +39,35 @@ public class LianJiaSpider extends Spider {
                 if (addresselements.size() == 1) {
                     LianjiaLogger.trace("整体房屋情况字串：" + addresselements.text());
                     String[] addressArray = addresselements.text().split("/");
-                    // 地址
-                    house.address = addressArray[0];
-                    // 面积(面积格式为  XX平方米，转为数字面积)
-                    house.area = this.getDigit(addressArray[1]);
-                    house.oriented = addressArray[2];
-                    house.housetype = addressArray[3];
-                    house.floor = addressArray[4];
+                    house.address = addressArray[0].replace(" ","");    // 地址
+                    house.area = this.getDigit(addressArray[1]);    // 面积(面积格式为 XX平方米，转为数字面积)
+                    house.oriented = addressArray[2].replace(" ","");
+                    house.housetype = addressArray[3].replace(" ","");
+                    house.floor = addressArray[4].replace(" ","");
                 } else {
                     LianjiaLogger.error("单条content__list--item中存在两个地址信息，查看网页源代码搂错误");
                     return houselist;
                 }
-
-                //寻找价格部分
+                // 获取价格部分（价格格式为 “XXXX 元/月”）
                 house.price = this.getDigit(element.getElementsByClass("content__list--item-price").text());
+                //获取高德location信息
+                house.GDlocation = gaode.getLocate("北京", house.address);
                 houselist.add(house);
-                LianjiaLogger.info("房源地址：" + house.address+"  面积 ："+house.area +" 价格："+house.price);
+                LianjiaLogger.info("房源地址：" + house.address + "  面积 ：" + house.area + " 价格：" + house.price+" GDlocation :"+house.GDlocation);
             }
         }
-        LianjiaLogger.info("本页共找到房源----"+houselist.size()+"个");
+        LianjiaLogger.info("本页共找到房源----" + houselist.size() + "个");
         return houselist;
     }
 
-    public Integer getDigit(String text){
+    public Integer getDigit(String text) {
         Integer result = null;
-        Pattern  p = Pattern.compile("[0-9]+");
+        Pattern p = Pattern.compile("[0-9]+");
         Matcher m = p.matcher(text);
-        if(m.find()){
-        result = Integer.parseInt(m.group());
-        }else{
-            LianjiaLogger.error("正则表达式匹配错误---匹配原字串为："+text);
+        if (m.find()) {
+            result = Integer.parseInt(m.group());
+        } else {
+            LianjiaLogger.error("正则表达式匹配错误---匹配原字串为：" + text);
             return null;
         }
         return result;
--- src/main/java/com/leathersheer/tools/SpiderUnit/PubToolUnit/GaoDeUnit.java
+++ src/main/java/com/leathersheer/tools/SpiderUnit/PubToolUnit/GaoDeUnit.java
@@ -5,6 +5,7 @@ import com.leathersheer.tools.SpiderUnit.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class GaoDeUnit {
     public static final Logger GaoDeLogger = LogManager.getLogger();
@@ -13,23 +14,28 @@ public class GaoDeUnit {
 
     public static void main(String[] args) {
         GaoDeLogger.trace("-----进入GaoDeUnit------");
-        new GaoDeUnit().getLocate("北京", "丰台-看丹桥-新华街七里");
+        new GaoDeUnit().getLocate("北京", "朝阳-首都机场-首都机场南路西里");
     }
 
     /**
      * city 城市中文名 address 具体到门牌号
      */
-    public void getLocate(String city, String address) {
+    public String getLocate(String city, String address) {
         String requestUrl = GaoDeLocateUrl + "address=" + address + "&key=" + GaoDeKey + "&city=" + city;
         Spider spider = new Spider();
-        JSONObject doc = spider.getContent(requestUrl, JSONObject.class);
+        String location = new String();
+        JSONObject json = spider.getContent(requestUrl, JSONObject.class);
         try {
-            for (int i = 0; i < Integer.parseInt((String)doc.get("count")); i++) {
-                
+            for (int i = 0; i < Integer.parseInt((String) json.get("count")); i++) {
+                JSONArray jsarray = json.getJSONArray("geocodes");
+                JSONObject jsarrayobj = jsarray.getJSONObject(i);
+                location = jsarrayobj.get("location").toString();
             }
         } catch (Exception e) {
-            GaoDeLogger.error("非数字输入");
+            GaoDeLogger.error("[9999]获取高德返回异常" + json);
+            return "地址异常";
         }
-        GaoDeLogger.trace(doc);
+        GaoDeLogger.trace(json);
+        return location;
     }
 }
