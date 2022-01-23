6c2f35790f68e7f1c1ff859a093a20e157485ed5
Base-PC\RubberFun <jy0001087@gmail.com>
2022-01-23 21:53:50 +0800

    开始Amenity逻辑编写

---
+++ src/main/java/com/leathersheer/tools/SpiderUnit/Servlets/AmenityServlet.java
@@ -0,0 +1,25 @@
+package com.leathersheer.tools.SpiderUnit.Servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="AmenityServlet",urlPatterns = "/Amenity")
+public class AmenityServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        req.getRequestDispatcher("/WEB-INF/jsp/amenity.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        req.getRequestDispatcher("/WEB_INF/jsp/amenity.jsp").forward(req,resp);
+    }
+}
