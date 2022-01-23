8018c45e8211f4d98c9fb13dd7d5e0aebaa77eca
Base-PC\RubberFun <jy0001087@gmail.com>
2022-01-23 22:45:09 +0800

    增加todo

--- src/main/java/com/leathersheer/tools/SpiderUnit/Servlets/AmenityServlet.java
+++ src/main/java/com/leathersheer/tools/SpiderUnit/Servlets/AmenityServlet.java
@@ -9,16 +9,15 @@ import java.io.IOException;
 
 @WebServlet(name="AmenityServlet",urlPatterns = "/Amenity")
 public class AmenityServlet extends HttpServlet {
-
+    //get方法仅用于实现jsp页面的访问
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         req.getRequestDispatcher("/WEB-INF/jsp/amenity.jsp").forward(req,resp);
     }
-
+    // 业务逻辑写在post方法里，再返回给amenity页面
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        // TODO  在这里实现爬虫爬取数据的功能
 
         req.getRequestDispatcher("/WEB_INF/jsp/amenity.jsp").forward(req,resp);
     }
