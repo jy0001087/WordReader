468358be2891e1f2934e5e5c4373afc7289ef00d
XiaoXin <jy0001087@gmail.com>
2022-04-12 09:27:33 +0800

    增加shuaigay的spider，但只能登录，不能模拟访问数据

--- src/main/java/com/leathersheer/tools/SpiderUnit/Shuaigay/ShuaigayFetcher.java
+++ src/main/java/com/leathersheer/tools/SpiderUnit/Shuaigay/ShuaigayFetcher.java
@@ -1,4 +1,29 @@
 package com.leathersheer.tools.SpiderUnit.Shuaigay;
 
+import com.leathersheer.tools.SpiderUnit.SpiderServer.Spider;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jsoup.nodes.Document;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShuaigayFetcher {
+    public static final Logger shuaigayLogger = LogManager.getLogger();
+
+    public void doFetch(String url){
+        Spider spider = new Spider();
+        Map<String,String> para= new HashMap<String,String>();
+        para.put("username","1123");
+        para.put("password","1234");
+        //spider.getCookieswithPara(url,para);
+        spider.getContentwithCookies(url,Document.class);
+    }
+
+    public static void main(String[] args){
+        ShuaigayFetcher sf=new ShuaigayFetcher();
+        //sf.doFetch("https://www.shuaigay6.com/member.php?mod=logging&action=login&loginsubmit=yes&loginhash=LhLvE&inajax=1");
+        sf.doFetch("https://www.shuaigay6.com/forum.php?mod=viewthread&tid=1536090&highlight=%C2%CC%C5%AB%D3%FB%CD%FB");
+    }
+
 }
--- src/main/java/com/leathersheer/tools/SpiderUnit/SpiderServer/Spider.java
+++ src/main/java/com/leathersheer/tools/SpiderUnit/SpiderServer/Spider.java
@@ -1,15 +1,29 @@
 package com.leathersheer.tools.SpiderUnit.SpiderServer;
 
 import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
+import org.json.JSONStringer;
 import org.jsoup.Jsoup;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Spider {
     public static final Logger spiderLogger = LogManager.getLogger();
@@ -30,9 +44,9 @@ public class Spider {
             httpGet.setHeader("User-Agent", "spider");
             response = httpclient.execute(httpGet);
 
-            spiderLogger.trace("----------访问URL信息------- ：  " + url);
+            spiderLogger.trace("----------访问URL信息------- ：  \n" + url);
 
-            spiderLogger.trace("----------head 信息如下----------");
+            spiderLogger.trace("----------response head 信息如下----------");
             Header[] heads = response.getAllHeaders();
             for (int i = 0; i < heads.length; i++) {
                 spiderLogger.trace(heads[i]);
@@ -44,7 +58,7 @@ public class Spider {
                     spiderLogger.info("得到html类型返回");
                 }
             }
-            spiderLogger.trace("----------head 信息结束----------");
+            spiderLogger.trace("----------response head 信息结束----------");
 
             String html = EntityUtils.toString(response.getEntity());
             spiderLogger.info("页面编码为 ： " + this.getEncoding(html) + "  " + response.getStatusLine());
@@ -63,6 +77,145 @@ public class Spider {
             default:
                 spiderLogger.error("未返回正确格式的response");
                 break;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                response.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return (T) doc;
+    };
+
+
+    /**
+     * 获取cookies
+     * @param url
+     * @param para
+     */
+    public void getCookieswithPara(String url, Map<String,String> para) {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        HttpPost httpPost = new HttpPost(url);
+        Map<String,String> cookies = new HashMap<>();
+        //设置访问参数
+        httpPost.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0");
+
+        List<NameValuePair> parameters = new ArrayList<NameValuePair>(0);
+        parameters.add(new BasicNameValuePair("username", "jy0001087"));
+        parameters.add(new BasicNameValuePair("password", "woai28552@"));
+
+        try {
+            // 构造一个form表单式的实体
+            UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(parameters);
+            // 将请求实体设置到httpPost对象中
+            httpPost.setEntity(formEntity);
+            response = httpclient.execute(httpPost);
+
+            spiderLogger.trace("----------访问URL信息------- ：  \n" + url);
+
+            spiderLogger.trace("----------response head 信息如下----------");
+            Header[] heads = response.getAllHeaders();
+            for (int i = 0; i < heads.length; i++) {
+                spiderLogger.trace(heads[i]);
+                if (heads[i].toString().contains("json")) {
+                    responseType = "json";
+                    spiderLogger.info("得到json类型返回");
+                } else if (heads[i].toString().contains("html")) {
+                    responseType = "html";
+                    spiderLogger.info("得到html类型返回");
+                }
+            }
+            Header[] cookiesHeader = response.getHeaders("Set-Cookie");
+            for(int i=0;i<cookiesHeader.length;i++){
+                //TODO 写cookei分解语句
+            }
+
+            spiderLogger.trace("----------response head 信息结束----------");
+
+            String html = EntityUtils.toString(response.getEntity());
+            spiderLogger.info("页面编码为 ： " + this.getEncoding(html) + "  " + response.getStatusLine());
+            spiderLogger.trace("----------原始页面信息如下----------");
+            spiderLogger.trace(new String(html.getBytes("UTF-8"), "UTF-8"));
+            spiderLogger.trace("----------原始页面信息结束----------");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                response.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public <T> T getContentwithCookies(String url, Class<T> type) {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        T doc = null;
+        BasicCookieStore cookieStore = new BasicCookieStore();
+        cookieStore.addCookie(new BasicClientCookie("jBy3_2132_saltkey","EGEuDPUA"));
+        cookieStore.addCookie(new BasicClientCookie("jBy3_2132_lastvisit","1649654727"));
+        cookieStore.addCookie(new BasicClientCookie("jBy3_2132_sid","OsXnWX"));
+        cookieStore.addCookie(new BasicClientCookie("jBy3_2132_lastact","1649670501%09home.php%09spacecp"));
+        cookieStore.addCookie(new BasicClientCookie("jBy3_2132_ulastactivity","8299Ho3g679aCepAQtBOwUuQaRnrj8WIpz7MQSx0BzHIYxpeovSZ"));
+        cookieStore.addCookie(new BasicClientCookie("jBy3_2132_auth","4eeasJUPghAVqaH%2FUnNuI8fzBhesomfmiyaq7719STUUpfdjXxwx44bM53c%2B%2BPPrzr11jSfNISAKlxFXIdpwu39uh4Er"));
+        cookieStore.addCookie(new BasicClientCookie("jBy3_2132_lastcheckfeed","1124819%7C1649670502"));
+        cookieStore.addCookie(new BasicClientCookie("jBy3_2132_lip","114.254.2.244%2C1649670224"));
+        cookieStore.addCookie(new BasicClientCookie("jBy3_2132_checkfollow","1"));
+        cookieStore.addCookie(new BasicClientCookie("jBy3_2132_checkpm","1"));
+        cookieStore.addCookie(new BasicClientCookie("jBy3_2132_viewid","tid_1536090"));
+        cookieStore.addCookie(new BasicClientCookie("jBy3_2132_st_p","1124819%7C1649670501%7C6568d531d060ebd33f122469e318899b"));
+        cookieStore.addCookie(new BasicClientCookie("jBy3_2132_seccodecSNT4E4t","168.ebae58b820aea91702"));
+
+
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            httpGet.setHeader("User-Agent", "spider");
+            httpGet.addHeader("Cookie",cookieStore.toString());
+
+            response = httpclient.execute(httpGet);
+
+            spiderLogger.trace("----------访问URL信息------- ：  \n" + url);
+
+            spiderLogger.trace("----------response head 信息如下----------");
+            Header[] heads = response.getAllHeaders();
+            for (int i = 0; i < heads.length; i++) {
+                spiderLogger.trace(heads[i]);
+                if (heads[i].toString().contains("json")) {
+                    responseType = "json";
+                    spiderLogger.info("得到json类型返回");
+                } else if (heads[i].toString().contains("html")) {
+                    responseType = "html";
+                    spiderLogger.info("得到html类型返回");
+                }
+            }
+            spiderLogger.trace("----------response head 信息结束----------");
+
+            String html = EntityUtils.toString(response.getEntity());
+            spiderLogger.info("页面编码为 ： " + this.getEncoding(html) + "  " + response.getStatusLine());
+            spiderLogger.trace("----------原始页面信息如下----------");
+            spiderLogger.trace(new String(html.getBytes("UTF-8"), "UTF-8"));
+            spiderLogger.trace("----------原始页面信息结束----------");
+
+            switch (responseType) {
+                case "html":
+                    doc = (T) Jsoup.parse(html);
+                    break;
+                case "json":
+                    JSONObject jsonObject = new JSONObject(html);
+                    doc = (T) jsonObject;
+                    break;
+                default:
+                    spiderLogger.error("未返回正确格式的response");
+                    break;
             }
 
         } catch (Exception e) {
@@ -113,37 +266,4 @@ public class Spider {
         return "";
     }
 
-    public static void main(String[] args){
-        String addresselements = "大兴-大兴其它-南海家园三里 / 66㎡ /南 北 / 1室1厅1卫 / 高楼层 （15层）";
-        String[] addressArray = addresselements.split("/");
-
-        for (String text:addressArray){
-            //spiderLogger.trace("现在处理："+text);
-            int house_element_order = 0;
-            house_element_order = text.matches(".+-+.+")?1:house_element_order;
-            house_element_order = text.matches(".+㎡+.+")?2:house_element_order;
-            house_element_order = text.matches(".+室+.+")?3:house_element_order;
-            house_element_order = text.matches("东?南?西?北?")?4:house_element_order;
-            house_element_order = text.matches(".+层+.+")?5:house_element_order;
-            switch(house_element_order){
-                case 1:
-                    spiderLogger.trace("地址："+text);
-                    break;
-                case 2:
-                    spiderLogger.trace("面积："+text);   // 面积(面积格式为 XX平方米，转为数字面积)
-                    break;
-                case 3:
-                spiderLogger.trace("户型："+text); //户型
-                    break;
-                case 4:
-                spiderLogger.trace("朝向："+text);//朝向
-                    break;
-                case 5:
-                spiderLogger.trace("楼层："+text);//楼层
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
 }
